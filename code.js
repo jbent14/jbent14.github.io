ba34f8ba5274d0055d75ef53a3f171e105dd2bad
jbent14 <jbent14@smpanthers.org>
2014-05-12 14:35:38 -0700

    added code.js

---
+++ code.js
@@ -0,0 +1,7 @@
+function changetext() {
+              $("#foo").html("I am addicted to video games, and spend too much time on them, I even skip homework everyday, just to play a game or watch Netflix.")
+              $("#foo").toggle();
+            }
+function picture() {
+                $("#photo").html('<img src="http://i66.photobucket.com/albums/h252/dcastine/1985%20El%20Camino/14789675-700-0.jpg">');
+            }
